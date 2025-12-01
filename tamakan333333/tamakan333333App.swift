e029a725b793220d704bcd3117221df81b487234
Nouran-salah-ali <nouransalahali@gmail.com>
2025-12-01 11:15:36 +0300

    Initial Commit

---
+++ tamakan333333/tamakan333333App.swift
@@ -0,0 +1,17 @@
+//
+//  tamakan333333App.swift
+//  tamakan333333
+//
+//  Created by nouransalah on 10/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct tamakan333333App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tamakan333333Tests/tamakan333333Tests.swift
@@ -0,0 +1,17 @@
+//
+//  tamakan333333Tests.swift
+//  tamakan333333Tests
+//
+//  Created by nouransalah on 10/06/1447 AH.
+//
+
+import Testing
+@testable import tamakan333333
+
+struct tamakan333333Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
